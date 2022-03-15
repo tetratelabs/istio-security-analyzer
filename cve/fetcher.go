8b0fbd379b98432f593280fe277dca80bc83b0a3
Jianfei Hu <hujianfei258@gmail.com>
2022-03-15 05:12:08 +0000

    mst cve fetcher get right content.
    Signed-off-by: Jianfei Hu <hujianfei258@gmail.com>

--- cve/fetcher.go
+++ cve/fetcher.go
@@ -19,12 +19,17 @@ func FetchIstioPage() error {
 	}
 
 	doc.Find(".security-grid table tbody tr").Each(func(i int, s *goquery.Selection) {
-		// fmt.Printf("jianfeih debug goqueyr find %v\n", i)
-		h, e := s.Html()
-		fmt.Printf("jianfeih content %v, %v\n", h, e)
-		// For each item found, get the title
-		// title := s.Find("a").Text()
-		// fmt.Printf("Review %d: %s\n", i, title)
+		a := s.ChildrenFiltered("td")
+		// NOTE: for affetcted release, Istio annoucement page uses a human readable format.
+		// Parsing and convert text to an array of the versions is fragile.
+		// Instead we can come up with a tool to update semi-automate:
+		// Find a new entry, give the text.
+		// Reminds the human, fill in the content. (may be a Web server form, or a CLI tool whichever easier.)
+		// Update the database.
+		// Next time seeing the entry, no new work requires.
+		a.Each(func(i int, elm *goquery.Selection) {
+			fmt.Printf("jianfeih element %v\n", elm.Text())
+		})
 	})
 	return nil
 }
